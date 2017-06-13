f378549ae379e90f6a6d035a2713e3a7a38b2ca9
sReNcAt <srenren1029@gmail.com>
2017-06-13 09:03:14 +0900

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var _ = require('lodash'),
+	glob = require('glob');
+
+module.exports.getGlobbedFiles = function(globPatterns, removeRoot) {
+	var _this = this;
+	var urlRegex = new RegExp('^(?:[a-z]+:)?\/\/', 'i');
+	var output = [];
+	if (_.isArray(globPatterns)) {
+		globPatterns.forEach(function(globPattern) {
+			output = _.union(output, _this.getGlobbedFiles(globPattern, removeRoot));
+		});
+	} else if (_.isString(globPatterns)) {
+		if (urlRegex.test(globPatterns)) {
+			output.push(globPatterns);
+		} else {
+            var files = glob(globPatterns, { sync: true });
+            if (removeRoot) {
+                files = files.map(function(file) {
+                    return file.replace(removeRoot, '');
+                });
+            }        
+            output = _.union(output, files);
+        }
+	}
+	return output;
+};
